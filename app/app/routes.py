bd6d576d4ad8b2bb135e80dea1879062562bd7c4
snmnmin <snmnmin@gmail.com>
2018-04-20 01:45:30 -0500

    change ui

--- app/app/routes.py
+++ app/app/routes.py
@@ -1,23 +1,35 @@
 from app import app
 from flask import render_template, flash, redirect, url_for
 from app.model.forms import SearchForm
-# from app import mongo
+import json
+import random
 import pandas as pd
 
-df = pd.read_csv("songcleaned.csv");
+df = pd.read_csv("songcleaned.csv")
 
 @app.route('/')
 @app.route('/index')
 def index():
-	example = {'song': "Yesterday, When I Was Mad", "artist": "Pet Shop Boys"}
-	return render_template('index.html', example=example)
+	index = random.sample(range(len(df)), 1)
+	data = df.ix[index].to_json(orient='records', lines=True)
+	data = json.loads(data)
+	# example = {'song': "Yesterday, When I Was Mad", "artist": "Pet Shop Boys"}
+	return render_template('index.html', example=data)
 
 @app.route('/search/<song>/<artist>', methods=['GET'])
 def search(song, artist):
-	
-	data = df.loc[(df['song']==song) & (df['artist']==artist)]
-	return data.to_json(orient='records')
+	data = df.loc[(df['song']==song.strip()) & (df['artist']==artist.strip())]
+	data = data.to_json(orient='records')
+	if len(data) == 0:
+		data = df.loc[(df['song']==song)].to_json(orient='records')
+	# print(data)
+	return data
 
 
-
-
+@app.route('/random', methods=['GET'])
+def generateRandom():
+	index = random.sample(range(len(df)), 1)
+	data = df.ix[index].to_json(orient='records', lines=True)
+	# data = json.loads(data)
+	# print(data)
+	return data
